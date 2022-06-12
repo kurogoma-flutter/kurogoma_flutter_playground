b0f20d991b91a73198f5a715fe54e77eaf982454
kurogoma939 <kan.sss.939@gmail.com>
2022-06-12 22:50:59 +0900

    material2と3の切り替え

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+final materialSwitchProvider = StateProvider((ref) => false);
+final counterProvider = StateProvider((ref) => 0);
+final bottomNavigationIndexProvider = StateProvider((ref) => 0);
+final navigationRailIndexProvider = StateProvider((ref) => 0);
+final titleProvider = Provider((ref) => "Flutter Demo");
 
-class MyApp extends StatelessWidget {
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blueGrey,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Material3'),
-          onPressed: () {},
+        debugShowCheckedModeBanner: false,
+        title: ref.watch(titleProvider),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: ref.watch(materialSwitchProvider), // Material3 有効
         ),
-      ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(ref.watch(titleProvider)),
+            actions: [
+              Row(children: [
+                Text("Material3"),
+                Switch(
+                  value: ref.watch(materialSwitchProvider),
+                  onChanged: (_) => ref
+                      .read(materialSwitchProvider.state)
+                      .update((state) => !state),
+                ),
+              ]),
+            ],
+          ),
+          body: Builder(builder: (context) {
+            final Size screenSize = MediaQuery.of(context).size;
+            final index = ref.watch(bottomNavigationIndexProvider);
+            return <Widget>[
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('You have pushed the button this many times:'),
+                    Text(ref.watch(counterProvider).toString(),
+                        style: Theme.of(context).textTheme.headline4),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  NavigationRail(
+                      selectedIndex: ref.watch(navigationRailIndexProvider),
+                      onDestinationSelected: (index) => ref
+                          .read(navigationRailIndexProvider.state)
+                          .update((state) => index),
+                      labelType: NavigationRailLabelType.selected,
+                      destinations: const <NavigationRailDestination>[
+                        NavigationRailDestination(
+                            icon: Icon(Icons.favorite_border),
+                            selectedIcon: Icon(Icons.favorite),
+                            label: Text('First')),
+                        NavigationRailDestination(
+                            icon: Icon(Icons.bookmark_border),
+                            selectedIcon: Icon(Icons.book),
+                            label: Text('Second')),
+                        NavigationRailDestination(
+                            icon: Icon(Icons.star_border),
+                            selectedIcon: Icon(Icons.star),
+                            label: Text('Third')),
+                      ]),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: screenSize.width * 0.1,
+                        right: screenSize.width * 0.1),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(height: 20),
+                        Card(
+                            child: Container(
+                                width: screenSize.width * 0.6,
+                                padding: EdgeInsets.all(30),
+                                child: Text("Card ウィジェット"))),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          child: Container(
+                              width: screenSize.width * 0.6 - 40,
+                              padding: EdgeInsets.all(30),
+                              child: Text(
+                                  "ElevatedButton ウィジェット\n（クリックしてAlertDialog）")),
+                          onPressed: () => _showAlertDialog(context),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ][index];
+          }),
+          floatingActionButton: ref.watch(bottomNavigationIndexProvider) == 0
+              ? FloatingActionButton(
+                  onPressed: () => ref
+                      .read(counterProvider.state)
+                      .update((state) => state + 1),
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                )
+              : null,
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: ref.watch(bottomNavigationIndexProvider),
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "ホーム"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.widgets), label: "ウィジェット")
+            ],
+            onTap: (index) => ref
+                .read(bottomNavigationIndexProvider.state)
+                .update((state) => index),
+          ),
+        ));
+  }
+
+  Future<void> _showAlertDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('アラートダイアログ'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('ダイアログメッセージ１行目'),
+                Text('ダイアログメッセージ２行目'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
